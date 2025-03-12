e1c897573c1f8415ae27ca7f639bd05208c7297d
nickchacha <nickchacha@gmail.com>
2025-03-12 23:11:14 +0300

    updated the codes

--- __manifest__.py
+++ __manifest__.py
@@ -2,14 +2,17 @@
     'name': 'Complete Debranding for Odoo 16',
     'version': '1.0',
     'depends': ['web', 'point_of_sale'],
-    'data': ['views/debranding_views.xml'],
+    'data': ['views/login_templates.xml'],
     'assets': {
     'web.assets_backend': [
-        'my_debranding/static/src/img/logo.png',
-        'my_debranding/static/src/js/debranding.js',
+        'change_theme/static/src/img/logo.png',
+        'change_theme/static/src/js/debranding.js',
+        'change_theme/static/src/pos/chrome.xml',
+        
     ],
     'point_of_sale.assets': [
-        'my_debranding/static/src/img/logo.png',
+        'change_theme/static/src/img/logo.png',
+
     ],
 },
     'installable': True,
